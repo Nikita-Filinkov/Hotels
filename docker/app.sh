3240b02437f5d639620719d49fb430aee91b29d6
Nikit <nikita_filinkov@mail.ru>
2025-07-10 21:54:16 +0300

    add healthcheck in docker-compose.yml

--- docker/app.sh
+++ docker/app.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-while ! nc -z db 5432; do
-  echo "Waiting for PostgreSQL to start..."
-  sleep 1
-done
+while ! nc -z db 5432; do sleep 1; done
 
 
 
 alembic upgrade head
-if [ $? -ne 0 ]; then
-    echo "Migration failed! Exiting..."
-    exit 1
-fi
 
-exec gunicorn app.main:app \
-    --workers 1 \
-    --worker-class uvicorn.workers.UvicornWorker \
-    --bind 0.0.0.0:8000
+gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
 #exec gunicorn app.main:app \
 #    --workers 4 \
 #    --worker-class uvicorn.workers.UvicornWorker \
