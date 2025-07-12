b1a148c4112a82f15268930e22140d9fa4facc4f
Nikit <nikita_filinkov@mail.ru>
2025-07-12 20:18:36 +0300

    changed app.sh to deploy to render.com

--- docker/app.sh
+++ docker/app.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-while ! nc -z db 5432; do sleep 1; done
+# Извлекаем хост БД из DATABASE_URL
+DB_HOST=$(echo $DATABASE_URL | awk -F'[@:]' '{print $4}')
 
+# Ожидание доступности PostgreSQL
+echo "Waiting for PostgreSQL at $DB_HOST..."
+while ! nc -z $DB_HOST 5432; do
+  sleep 1
+done
+echo "PostgreSQL is ready!"
 
-
+echo "Applying database migrations..."
 alembic upgrade head
+if [ $? -ne 0 ]; then
+  echo "Failed to apply migrations!"
+  exit 1
+fi
 
-gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+# Запуск приложения
+exec gunicorn app.main:app \
+    --workers 1 \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --bind 0.0.0.0:8000
+
+#while ! nc -z db 5432; do sleep 1; done
+#
+#
+#
+#alembic upgrade head
+#
+#gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
 #exec gunicorn app.main:app \
 #    --workers 4 \
 #    --worker-class uvicorn.workers.UvicornWorker \
