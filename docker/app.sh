782ff421cd666ab9cdd6eead9a9f3135fd6f346a
Nikit <nikita_filinkov@mail.ru>
2025-07-12 23:45:43 +0300

    changed app.sh to use for deploy

--- docker/app.sh
+++ docker/app.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-while ! nc -z db 5432; do sleep 1; done
+
+POSTGRES_HOST=$(echo "$DATABASE_URL" | sed -r 's/.*@([^:]+):.*/\1/')
+while ! pg_isready -h "$POSTGRES_HOST" -p 5432; do
+  echo "Waiting for PostgreSQL to start..."
+  sleep 1
+done
 
 
+if ! alembic upgrade head; then
+  echo "ALEMBIC MIGRATION FAILED!"
+  exit 1
+fi
 
-alembic upgrade head
+# Запуск приложения через Gunicorn
+exec gunicorn app.main:app \
+  --workers 1\
+  --worker-class uvicorn.workers.UvicornWorker \
+  --bind 0.0.0.0:8000 \
+  --timeout 120
 
-gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+#while ! nc -z db 5432; do sleep 1; done
+#
+#
+#
+#alembic upgrade head
+#
+#gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
 #exec gunicorn app.main:app \
 #    --workers 4 \
 #    --worker-class uvicorn.workers.UvicornWorker \
